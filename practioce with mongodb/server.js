29abafc32e3a1b081c4efcfd675ad9fcc7a8d285
hoangpn <phanngochoang1995@gmail.com>
2021-09-22 18:02:45 +0700

    add mongodb

--- practioce with mongodb/server.js
+++ practioce with mongodb/server.js
@@ -3,18 +3,8 @@ const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
-// morgan(function (tokens, req, res) {
-//   return [
-//     tokens.method(req, res),
-//     tokens.url(req, res),
-//     tokens.status(req, res),
-//     tokens.res(req, res, "content-length"),
-//     "-",
-//     tokens["response-time"](req, res),
-//     "ms",
-//   ].join(" ");
-// });
-// Make sure you place body-parser before your CRUD handlers!
+const connectionString =
+  "mongodb+srv://hoang123:hoang123@cluster0.9xd3z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(3000, function () {
   console.log("listening on 3000");
@@ -26,3 +16,22 @@ app.get("/", function (req, res) {
 app.post("/quotes", (req, res) => {
   console.log(req.body);
 });
+MongoClient.connect(
+  connectionString,
+  {
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) return console.error(err);
+    console.log("Connected to Database");
+    const db = client.db("tutorial");
+    app.post("/quotes", (req, res) => {
+      quotesCollection
+        .insertOne(req.body)
+        .then((result) => {
+          console.log(result);
+        })
+        .catch((error) => console.error(error));
+    });
+  }
+);
